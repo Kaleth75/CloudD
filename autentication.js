98bd10274cb348e23b13c7a0550a642cffca89dd
Kaleth75 <129777432+Kaleth75@users.noreply.github.com>
2024-03-30 23:46:16 -0500

    Add files via upload

---
+++ autentication.js
@@ -0,0 +1,34 @@
+import { login_auth } from "./global.js";
+
+const evento = document.getElementById("login_btn");
+
+export async function validar() {
+    const email = document.getElementById('usuario').value;
+    const password = document.getElementById('password').value;
+
+    if (email.trim() === '' || password.trim() === '') {
+        alert("Debe llenar los campos de usuario y el de contraseña.");
+        return;
+    }
+
+    try {
+        const verification = await login_auth(email, password);
+
+        if (verification != null) {
+            alert("Usuario registrado " );
+            window.location.href = "Templates/home.html";
+        } else {
+            console.log("Sesión " + email + " no valida");
+            alert("Error de usuario, verifique usuario y/o contraseña.");
+        }
+    } catch (error) {
+        console.error("Error al autenticar:", error);
+        alert("Error de usuario, verifique usuario y/o contraseña.");
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    evento.addEventListener('click', validar);
+  
+});
+
